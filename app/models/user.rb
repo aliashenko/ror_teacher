743f28449b73cd67e2d2373b61ab7118daa6cae5
aliashenko <lyashenko.ann@gmail.com>
2013-10-29 17:31:59 +0200

    Add user_type check

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_many :courses_users
   has_many :courses, through: :courses_users
+
+  def is_student?
+    user_type == "student"
+  end
+
+  def is_teacher?
+    user_type == "teacher"
+  end
+
 end
