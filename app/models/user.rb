c23a63e541949216ecb258dc4ac0c37d3cba67f7
aliashenko <lyashenko.ann@gmail.com>
2013-10-30 18:06:01 +0200

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_many :courses_users
   has_many :courses, through: :courses_users
+  validates :first_name, :last_name, :user_type, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence:   true,
+                    format:     { with: VALID_EMAIL_REGEX }
 
   def is_student?
     user_type == "student"
