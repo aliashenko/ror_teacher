3d34384d37e2a47c27dc3caa48f5a8f661095870
aliashenko <lyashenko.ann@gmail.com>
2013-11-15 09:19:46 +0200

    Change authentication method

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Ror Teacher"
+  config.site_title = "RoR Teacher"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
@@ -55,7 +55,7 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # within the controller.
-  config.authentication_method = :authenticate_user!
+  config.authentication_method = :authenticate_admin_user!
   # == User Authorization
   #
   # Active Admin will automatically call an authorization
@@ -100,7 +100,7 @@ ActiveAdmin.setup do |config|
   # link. For example :get, :delete, :put, etc..
   #
   # Default:
-  # config.logout_link_method = :get
+  config.logout_link_method = :delete
 
 
   # == Root
