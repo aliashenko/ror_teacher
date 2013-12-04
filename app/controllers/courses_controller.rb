c671aef026a6f8fe47c009b3677392477391f972
anna <lyashenko.ann@gmail.com>
2013-12-04 10:45:22 +0200

    Add Course pages

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,10 +2,11 @@ class CoursesController < ApplicationController
 
   def index
     @courses = Course.all.order( name: :asc )
+    @user = current_user
   end
 
   def show
-
+    @course = Course.find(params[:id])
   end
 
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,8 @@ class Course < ActiveRecord::Base
   has_many :users, through: :courses_users
   has_many :pages
   has_many :assignments
+
+  def is_public?
+    public == true
+  end
 end
