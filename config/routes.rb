18ef6bffdf0dced806056a5ccaaabb44d25e975e
anna <lyashenko.ann@gmail.com>
2013-12-09 14:11:06 +0200

    Some fix in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 RorTeacher::Application.routes.draw do
 
   ActiveAdmin.routes(self)
-  devise_for :users
-  resources :users
-  resources :courses
-  resources :courses_user
+  devise_for  :users
+  resources   :users
+  resources   :courses
+  resources   :courses_user
 
   root  'courses#index'
-  get   '/',                    to: 'courses#index',      as: "home"
+  get   '/',                    to: 'courses#index',      as: 'home'
   get   '/about',               to: 'static_pages#about'
   get   '/contact',             to: 'static_pages#contact'
   post  '/courses_user/start',  to: 'courses_user#start'
