9144d3dba31e18f264c9313afb54531b953230d3
aliashenko <lyashenko.ann@gmail.com>
2013-12-19 17:10:08 +0200

    Add specs for #index and #show actions of UsersController

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,20 +1,52 @@
 require 'spec_helper'
+require 'database_cleaner'
 
 describe UsersController do
 
-  let(:teacher) { FactoryGirl.create(:user) }
+  let(:teacher) { FactoryGirl.create(:user, user_type: 'teacher') }
   let(:student) { FactoryGirl.create(:user) }
 
-  before { controller.stub(current_user: student) }
+  before { controller.stub(:current_user).and_return(student) }
 
   describe "GET #index" do
-    it "GET #index" do
-      get :index
+    before { get :index }
+
+    it "returns success response" do
       expect(response).to be_success
     end
 
+    it "renders the index template" do
+      expect(response).to render_template("index")
+    end
+
     it "contains only students" do
-      expect(@students).to include(student)
+      expect(assigns[:students]).to include(student)
+    end
+
+    it "doesn't contain teachers" do
+      expect(assigns[:students]).not_to include(teacher)
+    end
+  end
+
+  describe "GET #show/:id" do
+    let(:course)  { FactoryGirl.create(:course) }
+    before { get :show, id: student }
+
+    it "returns sucess response" do
+      expect(response).to be_success
+    end
+
+    it "renders the show template" do
+      expect(response).to render_template("show")
+    end
+
+    it "shows correct user" do
+      expect(assigns[:user]).to eq(student)
+    end
+
+    it "shows courses for user" do
+      student.courses << course
+      expect(assigns[:courses]).to include(course)
     end
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -2,10 +2,16 @@ FactoryGirl.define do
   factory :user do
     sequence(:first_name) { |n| "First #{n}" }
     sequence(:last_name)  { |n| "Last #{n}" }
-    email                 Faker::Internet.email
+    sequence(:email)      { |n| "test_email#{n}@mail.com" }
+    # email                 Faker::Internet.email
     user_type             'student'
     admin                 false
     password              'password'
     password_confirmation 'password'
+  end
+
+  factory :course do
+    name                  Faker::Lorem.word.capitalize!
+    public                true
   end
 end
